7d7a2450df66b6db887f3f1bc60f572666bc802f
crispinalan <crispinalan@gmail.com>
2024-02-12 11:57:59 +0000

    Talker version 0.1.4
    [build]: C and Gtk4 (Debian 12 Bookworm)
    [feat]: text-to-speech diphone synthesizer
    [feat]: text input user interface
    [feat]: binary for 64-bit Debian distributions
    [docs]: readme documentation updated

--- README.md
+++ README.md
@@ -1,83 +1,85 @@
-# Diphone Speech Synthesizer (talkdp)
+# Talker (Diphone Speech Synthesizer)
 
-The application talkdp (dp for diphone) is a Linux command line diphone speech synthesizer. It has been written in C++. 
+Talker is a diphone speech synthesizer. It has been developed using C and [GTK4](https://docs.gtk.org/gtk4/). A screenshot is shown below.
+
+![](talker.png) 
 
 ### Prebuilt Binary
 
-A 64 bit prebuilt binary for talkdp is available and can be downloaded from the binary folder. This has been built using Debian 12 Bookworm. Download and extract. The talkdp executable can be run from a terminal.
+A 64 bit prebuilt binary is available and can be downloaded from the binary folder. This has been built using Debian 12 Bookworm. Download and extract. The talker executable can be run from a terminal.
 
 ```
-./talkdp
+./talker
 ```
 
-A screenshot of talkdp is shown below.
-
-![](talkdp.png)
-
-
-The talkdp must have executable permissions. Use the command below if necessary.
+Talker must have executable permissions. Use the command below if necessary.
 
 ```
-chmod +x talkdp
+chmod +x talker
 ```
-The diphone directory should be located in the current working directory.
-
-## How is Speech Generated?
-
-Words are formed as sequences of elementary speech units. A phoneme is the smallest unit of sound that distinguishes one word from another word and there are 44 phonemes in the English language. A diphone is a sound unit composed of two adjacent partial phonemes i.e. the second half of the first phoneme and the first half of the second phoneme. The synthesizer uses a set of pre-recorded diphone sound samples and concatenates diphone wav files to produce speech output for a given text input.
-
-The folder containing the diphone wav files should be placed into the application binary (executable) working directory. The diphone collection was created by Alan W Black and Kevin Lenzo and more information can be found using the links in the Acknowledgements. The diphone license can be found [here](https://github.com/hypnaceae/DiphoneSynth/blob/master/diphones_license.txt).
-
-The diphone speech synthesizer uses a small dictionary of approximately 56,600 English words. If a word is not recognised by the dictionary it is skipped over. More words will be added in future updates.
-
 ## Audio
 
-I have used the Advanced Linux Sound Architecture (ALSA) library [aplay](https://alsa.opensrc.org/Aplay) program for audio output. If building a desktop from a Debian base install you will need to install ALSA as shown below but otherwise this is installed by default.
+I have used the Advanced Linux Sound Architecture (ALSA) library [aplay](https://alsa.opensrc.org/Aplay) program for audio output. If building a desktop from a Debian base install you will need to install the ALSA utilities as shown below but otherwise this is installed by default.
 
 ```
 sudo apt install alsa-utils
 ```
 
-Debian 12 GNOME provides Pipewire as the default audio server. Pipewire aims to improve audio and video handling in Linux systems replacing PulseAudio. To verify the version of Pipewire installed, run the command:
-
-```
-pipewire --version
-```
-
-I originally used [pw-play](https://www.thegeekdiary.com/pw-play-command-examples-in-linux/) to play audio output but found that PipeWire was not installed by default when using Debain Xfce and reverted to using ALSA aplay
-
 ## Sample Phrases
 
-Pre-processing of input text is very limited at this stage just removing unwanted characters such as commas, exclamation marks and question marks. Some sample phrases for testing talkdp are shown below
+Pre-processing of input text is very limited at this stage just removing unwanted characters such as commas, exclamation marks and question marks. Some sample phrases for testing  the diphone speech synthesizer are shown below
 
 ```
-Hello world I am a diphone speech synthesizer 
 
-I am a text to speech converter
+Hello world I am a text to speech synthesizer
 
-Mary had a little lamb, It's fleece was white as snow And everywhere that Mary went? The lamb was sure to go!
+Mary had a little lamb, It's fleece was white as snow And everywhere that Mary went? The lamb was sure to go! 
 
-Talk diphone speech synthesizer wishes you a merry christmas and a happy new year.
+Wishing you a happy birthday.
+
 ```
+## How is Speech Generated?
+
+Words are formed as sequences of elementary speech units. A phoneme is the smallest unit of sound that distinguishes one word from another word and there are 44 phonemes in the English language. A diphone is a sound unit composed of two adjacent partial phonemes i.e. the second half of the first phoneme and the first half of the second phoneme. 
+
+This voice used by Diphone Talker is derivative work based on the diphone collection created by Alan W Black and Kevin Lenzo which is free for use for any purpose (commercial or otherwise) and subject to the pretty light restrictions detailed [here](https://github.com/hypnaceae/DiphoneSynth/blob/master/diphones_license.txt). I have used the same licence for the voice that I have created. There is information about recording your own diphones [here](http://festvox.org/bsv/x2401.html) and in the speech synthesis lecture by Professor Alan W Black [here](https://www.youtube.com/watch?v=eDjtEsOvouM&t=1459s).
+
+The diphone speech synthesizer uses a small dictionary of approximately 56,600 English words. If a word is not recognised by the dictionary it is skipped over. More words will be added in future updates.
 
 ## Build From Source
 
-The C++ source code for talkdp is provided in the src directory.
+The C source code for talker is provided in the src directory.
 
-With both  Debian Bookworm and Ubuntu and you need to install the following packages to compile talkdp.
+With both  Debian Bookworm and Ubuntu and you need to install the following packages to build Talker.
 
 ```
-sudo apt install build-essential
-sudo apt install cmake
+apt install build-essential
+apt install libgtk-4-dev
+apt install libasound2-dev
 ```
 
-Make a build directory and use the cd command to change into this. Then run cmake followed by make as shown below.
+The packages:
+
 ```
-mkdir build
-cd build
-cmake ..
+apt install libglib2.0-dev
+apt install alsa-utils
+```
+
+are needed but should be installed by default.
+
+Use the MAKEFILE to compile. 
+
+```
 make
 ```
+
+To run Talker from the terminal use
+
+```
+./talker
+```
+
+I have used Geany for developing the code which is a lightweight source-code editor with an integrated terminal. I have been using the [Budgie desktop ](https://buddiesofbudgie.org/) with Debian 12 Bookworm. See acknowledgements below.
 
 ## Versioning
 
@@ -89,7 +91,7 @@ make
 
 ## License
 
-The diphone speech synthesizer called talkdp is licensed under LGPL v2.1. 
+Talker is licensed under LGPL v2.1. 
 
 ## Project Status
 
@@ -97,9 +99,21 @@ Active.
 
 ## Acknowledgements
 
-* [Diphone Source and License](https://github.com/hypnaceae/DiphoneSynth/blob/master/diphones_license.txt)
+* [GTK](https://www.gtk.org/)
 
-* Diphone collection and synthesis Alan W. Black and Kevin Lenzo [2000](https://www.cs.cmu.edu/~awb/papers/ICSLP2000_diphone.pdf)
+* GTK is a free and open-source project maintained by GNOME and an active community of contributors. GTK is released under the terms of the [GNU Lesser General Public License version 2.1](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html).
 
+* [GTK4 API](https://docs.gtk.org/gtk4/index.html)
 
+* [GObject API](https://docs.gtk.org/gobject/index.html)
+
+* [Glib API](https://docs.gtk.org/glib/index.html)
+
+* [Gio API](https://docs.gtk.org/gio/index.html)
+
+* [Geany](https://www.geany.org/) is a lightweight source-code editor (version 2 now uses GTK3). [GPL v2 license](https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
+
+* Diphone collection and synthesis Alan W. Black and Kevin Lenzo [2000](https://www.cs.cmu.edu/~awb/papers/ICSLP2000_diphone.pdf) with license found [here](https://github.com/hypnaceae/DiphoneSynth/blob/master/diphones_license.txt)
+
+* How to Install Budgie Desktop on Debian. See [here](https://www.linuxcapable.com/how-to-install-budgie-desktop-on-debian-linux/) and [here](https://packages.debian.org/bookworm/budgie-desktop).
 
---
+++ src/main.c
@@ -0,0 +1,414 @@
+/***************************************************************************
+ *   Author Alan Crispin                                                   *
+ *   crispinalan@gmail.com                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation.                                         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include <gtk/gtk.h>
+#include "dictionary.h"
+#include "diphone.h"
+
+static void callbk_quit(GSimpleAction* action,G_GNUC_UNUSED GVariant *parameter, gpointer user_data);
+static void callbk_about(GSimpleAction* action, GVariant *parameter, gpointer user_data);
+static GMenu *create_menu(const GtkApplication *app);
+
+static int m_talk_rate=16000;
+
+//--------------------------------------------------------------------
+// Removers (unwanted characters}
+//--------------------------------------------------------------------
+
+static char *remove_commas(const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new("");
+	p = text;
+	while (*p)
+	{
+		gunichar cp = g_utf8_get_char(p);
+		if (cp != ',')
+		{ 
+			g_string_append_unichar(str, *p);
+		} // if
+		++p;
+	}
+	return g_string_free(str, FALSE);
+}
+
+static char *remove_fullstop(const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new("");
+	p = text;
+	while (*p)
+	{
+		gunichar cp = g_utf8_get_char(p);
+		if (cp != '.')
+		{ 
+			g_string_append_unichar(str, *p);
+		} // if
+		++p;
+	}
+	return g_string_free(str, FALSE);
+}
+static char* remove_semicolons (const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new ("");
+	p = text;
+	while (*p)
+	{
+	gunichar cp = g_utf8_get_char(p);
+	if ( cp != ';' ){ 
+	g_string_append_unichar (str, *p);
+	}//if
+	++p;
+	}
+	return g_string_free (str, FALSE);
+}
+
+static char* remove_question_marks (const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new ("");
+	p = text;
+	while (*p)
+	{
+	gunichar cp = g_utf8_get_char(p);
+	if ( cp != '?' ){ 
+	g_string_append_unichar (str, *p);
+	}//if
+	++p;
+	}
+	return g_string_free (str, FALSE);
+}
+
+static char* remove_explanation_marks (const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new ("");
+	p = text;
+	while (*p)
+	{
+	gunichar cp = g_utf8_get_char(p);
+	if ( cp != '!' ){ 
+	g_string_append_unichar (str, *p);
+	}//if
+	++p;
+	}
+	return g_string_free (str, FALSE);
+}
+
+static char* remove_punctuations(const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new ("");
+	p = text;
+	while (*p)
+	{
+	gunichar cp = g_utf8_get_char(p);
+	if ( cp != '\'' ){ //remove all apostrophes
+	g_string_append_unichar (str, *p);
+	}//if
+	++p;
+	}
+	return g_string_free (str, FALSE);
+}
+
+static char* replace_hypens(const char *text)
+{
+	GString *str;
+	const char *p;
+	str = g_string_new ("");
+	p = text;
+	gint i=0;
+	while (*p)
+	{
+	gunichar cp = g_utf8_get_char(p);
+	if ( cp != '-' ){ //append
+	g_string_append_unichar (str, *p);
+	}//if
+	if ( cp == '-' ){ //replace			
+	g_string_insert_unichar (str,i,' ');
+	}//if	
+	++p;
+	++i;
+	}
+	return g_string_free (str, FALSE);
+}
+
+//--------------------------------------------------------------------
+//play
+//--------------------------------------------------------------------
+void  playraw(gpointer user_data)
+{   
+    gchar *raw_file =user_data;       
+    gchar *m_sample_rate_str = g_strdup_printf("%i", m_talk_rate); 
+    gchar *sample_rate_str ="-r ";    
+    sample_rate_str= g_strconcat(sample_rate_str,m_sample_rate_str, NULL);     
+    gchar * command_str ="aplay -c 1 -f S16_LE";
+    command_str =g_strconcat(command_str," ",sample_rate_str, " ", raw_file, NULL); 
+    system(command_str); 
+}
+
+//--------------------------------------------------------------------
+//concatenate
+//--------------------------------------------------------------------
+
+unsigned char *rawcat(unsigned char *arrys[], unsigned int arry_size[], int arry_count) {
+		
+	
+	if (arry_count<2) return NULL;	
+	
+	unsigned int  total_samples=0;
+	for (int c = 0; c < arry_count; c++) 
+	{  
+    unsigned int count =arry_size[c]; 
+    total_samples=total_samples+count;	
+    }        
+	unsigned char *data = (unsigned char*)malloc(total_samples * sizeof(unsigned char));
+	
+	unsigned int offset=0;
+	for(int k=0; k<arry_count; k++)
+	{
+		//loop through each arry	
+		for(int i = 0; i < arry_size[k]; i++) 
+		{		
+		data[i+offset]=arrys[k][i];
+		}		
+		offset =offset+arry_size[k];
+	}//k kount
+	return data;
+}
+
+unsigned int get_merge_size(unsigned int sizes_arry[], int arry_size){
+	
+	unsigned int total_samples=0;
+	for (int i = 0; i < arry_size; i++) 
+	{  
+    unsigned int count =sizes_arry[i]; 
+    total_samples=total_samples+count;	
+    }
+	return total_samples;
+}
+
+//--------------------------------------------------------------------
+// speak button
+//--------------------------------------------------------------------
+static void button_speak_clicked (GtkButton *button, gpointer   user_data)
+{
+	GtkEntryBuffer *buffer_text; 
+	GtkWidget *entry_text = g_object_get_data(G_OBJECT(button), "button-entry-key");
+	buffer_text = gtk_entry_get_buffer (GTK_ENTRY(entry_text));	
+	const char* input_text= gtk_entry_buffer_get_text (buffer_text);
+	const char *text =g_ascii_strdown(input_text, -1);	//lower	
+	
+	//very simple input text processing
+	text = remove_semicolons(text);
+	text = remove_commas(text);
+	text = remove_fullstop(text);
+	text = remove_question_marks(text);	
+	text= remove_explanation_marks(text);
+	text =remove_punctuations(text);
+	text = replace_hypens(text); //replace hypens with space
+	
+	//g_print("processed text = %s\n", text);
+	
+	gchar** words;	
+	GList *diphone_list = NULL;			
+	words = g_strsplit (text, " ", 0); //split on space	
+	int j=0; //count			   
+	while(words[j] != NULL)
+	{	
+	//g_print("word =%s\n",words[j]);
+	diphone_list =g_list_concat(diphone_list,word_to_diphones(words[j]));
+	//diphone_list =g_list_concat(diphone_list, word_to_diphones("pau"));
+	j++;
+	} //while loop words
+		
+	gpointer diphone_list_pointer;
+	gchar* diphone_str;		
+	gint diphone_number  =g_list_length(diphone_list);
+	//g_print("diphone_number = %i\n", diphone_number);
+	//create diphone array using list length
+	unsigned char *diphone_arrays[diphone_number]; 
+	unsigned int diphone_arrays_sizes[diphone_number];
+		
+	//load diphone arrays
+	for(int i=0; i < diphone_number; i++)
+	{
+		diphone_list_pointer=g_list_nth_data(diphone_list,i);
+		diphone_str=(gchar *)diphone_list_pointer;					
+		//g_print("diphone = %s\n",diphone_str);		
+		diphone_arrays[i] = load_diphone_arry(diphone_str);
+		diphone_arrays_sizes[i]=load_diphone_len(diphone_str);		
+	}	
+	
+	//concatenate using raw cat
+	unsigned char *data = rawcat(diphone_arrays, diphone_arrays_sizes, diphone_number);	
+	unsigned int data_len = get_merge_size(diphone_arrays_sizes,diphone_number);	
+    
+    gchar* raw_file ="talkout.raw";
+	FILE* f = fopen(raw_file, "w");
+    fwrite(data, data_len, 1, f);
+    fclose(f);  
+    playraw(raw_file);
+	free(data);	//prevent memory leak
+	
+}
+
+//--------------------------------------------------------------------
+// callbk actions
+//--------------------------------------------------------------------
+
+static void callbk_quit(GSimpleAction * action,	G_GNUC_UNUSED GVariant *parameter, gpointer user_data)
+{
+	g_application_quit(G_APPLICATION(user_data));		
+}
+
+static void callbk_about(GSimpleAction* action, GVariant *parameter, gpointer user_data)
+{
+	GtkWidget *window = user_data;
+	const gchar *authors[] = {"Alan Crispin", NULL};
+	GtkWidget *about_dialog;
+	about_dialog = gtk_about_dialog_new();
+	gtk_window_set_transient_for(GTK_WINDOW(about_dialog),GTK_WINDOW(window));
+	gtk_widget_set_size_request(about_dialog, 200,200);
+    gtk_window_set_modal(GTK_WINDOW(about_dialog),TRUE);
+	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about_dialog), "Talker");
+	gtk_about_dialog_set_version (GTK_ABOUT_DIALOG(about_dialog), "0.1.4");
+	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about_dialog),"Copyright © 2024");
+	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(about_dialog),"Diphone Speech Synthesizer");
+	gtk_about_dialog_set_license_type (GTK_ABOUT_DIALOG(about_dialog), GTK_LICENSE_LGPL_2_1);
+	gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(about_dialog),"https://github.com/crispinprojects/");
+	gtk_about_dialog_set_website_label(GTK_ABOUT_DIALOG(about_dialog),"Project Website");
+	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(about_dialog), authors);
+	gtk_about_dialog_set_logo_icon_name(GTK_ABOUT_DIALOG(about_dialog), "x-office-calendar");
+	gtk_widget_set_visible (about_dialog, TRUE);	
+}
+
+//--------------------------------------------------------------------
+// menu
+//--------------------------------------------------------------------
+
+static GMenu *create_menu(const GtkApplication *app) {
+	
+		
+	GMenu *menu;
+    GMenu *file_menu; 
+    GMenu *help_menu;
+    GMenuItem *item;
+
+	menu =g_menu_new();
+	file_menu =g_menu_new();
+	help_menu =g_menu_new();
+	
+	//File item	
+	item =g_menu_item_new("Quit", "app.quit"); //quit action
+	g_menu_append_item(file_menu,item);
+	g_object_unref(item);
+			
+	//Help item	
+	item =g_menu_item_new("About", "app.about"); //about action
+	g_menu_append_item(help_menu,item);
+	g_object_unref(item);
+	
+	g_menu_append_submenu(menu, "File", G_MENU_MODEL(file_menu));
+    g_object_unref(file_menu);  
+   
+    g_menu_append_submenu(menu, "Help", G_MENU_MODEL(help_menu));
+    g_object_unref(help_menu);
+    
+    return menu;
+}
+
+
+//--------------------------------------------------------------------
+// activate
+//--------------------------------------------------------------------
+
+static void activate (GtkApplication* app, gpointer user_data)
+{
+	GtkWidget *window;			
+	GtkWidget *vbox;  
+	GtkWidget *label_text;  
+	GtkEntryBuffer *buffer; 
+	GtkWidget *entry_text;
+	GtkWidget *button_speak;
+	GMenu *menu;	
+		
+	//setup window
+	window = gtk_application_window_new (app);
+	gtk_window_set_title (GTK_WINDOW (window), "Text To Speech");
+	gtk_window_set_default_size (GTK_WINDOW (window),400,100);
+	//setup layout box
+	vbox =gtk_box_new(GTK_ORIENTATION_VERTICAL,1);  
+	gtk_window_set_child (GTK_WINDOW (window), vbox);
+	//actions
+	GSimpleAction *quit_action;	
+	quit_action=g_simple_action_new("quit",NULL); //app.quit
+	g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(quit_action)); //make visible	
+	g_signal_connect(quit_action, "activate",  G_CALLBACK(callbk_quit), app);
+	
+	GSimpleAction *about_action;
+	about_action=g_simple_action_new("about",NULL); //app.about
+	g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(about_action)); //make visible
+	g_signal_connect(about_action, "activate",  G_CALLBACK(callbk_about), window);
+	
+	//menu
+	menu=create_menu(app);	
+	gtk_application_set_menubar (app,G_MENU_MODEL(menu));
+    gtk_application_window_set_show_menubar(GTK_APPLICATION_WINDOW(window), TRUE);
+	
+	
+	//create label
+	label_text =gtk_label_new("Enter text ");
+	    
+	entry_text =gtk_entry_new(); 	
+	gtk_widget_set_hexpand (GTK_WIDGET (entry_text), true);
+		
+	button_speak = gtk_button_new_with_label ("Speak");
+	g_signal_connect (GTK_BUTTON (button_speak),"clicked", G_CALLBACK (button_speak_clicked), G_OBJECT (window));
+	
+	g_object_set_data(G_OBJECT(button_speak), "button-entry-key",entry_text);
+	
+	gtk_box_append(GTK_BOX(vbox), label_text); 
+    gtk_box_append(GTK_BOX(vbox), entry_text);
+	gtk_box_append(GTK_BOX(vbox), button_speak);
+	
+	gtk_window_present(GTK_WINDOW (window));    //use present not show with gtk4	
+}
+
+//--------------------------------------------------------------------
+// main
+//--------------------------------------------------------------------
+
+int main (int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.diphonetalker", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
